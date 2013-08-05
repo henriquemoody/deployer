123158a530007d47924f9fbe5c8492d3e49ac9e0
Henrique Moody <henriquemoody@gmail.com>
2013-08-05 14:31:19 -0300

    Fix wrong PID number in lock file

--- src/lock/create.bash
+++ src/lock/create.bash
@@ -1,5 +1,5 @@
 _lock_create()
 {
-    echo ${$} | sudo tee "${DEPLOY_LOCK_FILE}" > /dev/null
+    echo ${SCRIPT_PID} | sudo tee "${DEPLOY_LOCK_FILE}" > /dev/null
     sudo chmod 0600 "${DEPLOY_LOCK_FILE}"
 }
