cecef7d26f38bfe29fb35901d6e127563a62630b
Henrique Moody <henriquemoody@gmail.com>
2013-08-18 02:24:30 -0300

    Uniformize exit codes
    - Create variable SCRIPT_INTERACTIVE to check if the script is running
      in an interactive shell;
    - Update deploy log file names to use datetime instead of timestamp;
    - Create function `_exit()` to do logging with all exit codes;
    - Checks if DEPLOY_LOG_FILENAME variable exists on `_log` function;
    - Check variable DEPLOY_LOCK_FILE on `_lock_remove` function and
      remove logs of this function in favor of `_exit` function logs;
    - Implement logs on `_lock_create` function.

--- src/lock/create.bash
+++ src/lock/create.bash
@@ -1,5 +1,10 @@
 _lock_create()
 {
-    echo ${SCRIPT_PID} | sudo tee "${DEPLOY_LOCK_FILE}" > /dev/null
+    echo ${SCRIPT_PID} |
+        sudo tee "${DEPLOY_LOCK_FILE}" > /dev/null
+
     sudo chmod 0600 "${DEPLOY_LOCK_FILE}"
+
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] ${DEPLOY_USER}: ${DEPLOY_TYPE} of ${GIT_TREE_ISH} on ${ENVIRONMENT}" |
+        sudo tee -a "${DEPLOYER_LOG}" > /dev/null
 }
